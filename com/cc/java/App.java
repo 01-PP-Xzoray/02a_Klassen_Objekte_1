a8caf83e9ffaac40de201b231abd2441875230c2
Sohrab <sohrab-arenja8@hotmail.de>
2023-07-26 12:22:00 +0200

    this

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -12,14 +12,18 @@ public class App {
         System.out.println("Blick von aussen: " + cat1);
         cat1.tellYourAddress();
 
+        cat1.testObj();
+        Cat.testClass();
+
+        Math.abs(-1);
+
         System.out.println("----------------");
         
-        Cat cat2 = new Cat();
-        System.out.println("Blick von aussen: " + cat2);
-        cat2.tellYourAddress();
+        // Cat cat2 = new Cat();
+        // System.out.println("Blick von aussen: " + cat2);
+        // cat2.tellYourAddress();
     }
 
    
 
 }
-
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,9 +2,18 @@ package com.cc.java;
 
 public class Cat {
     
-// this --> Instanzvariable
+// this --> Instanzvariable & Objektgebunden
 
 public void tellYourAddress() {
     System.out.println("Blick von innen: " + this);
 }
+
+public void testObj() {
+    System.out.println("Grüße aus dem Objekt " + this);
+}
+
+public static void testClass() {
+    System.out.println("Grüße aus der Klasse!");
+}
+
 }
